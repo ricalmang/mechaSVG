c37d00197954910c3f67c830ba21c3bf0dba923a
Ricardo Almir Angnes <ricardo_almir@hotmail.com>
2020-09-17 20:30:12 -0300

    Handling of xlsx and txt files added

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.4",
     author="Ricardo Almir Angnes",
     author_email="ricardo_almir@hotmail.com",
-    description="mechaSVG is a python & tk application for creating good-looking energy profile diagrams as Scalable Vector Graphics or '.svg' files with various aesthetic options. The produced graphics can also be easily edited afterwards via an svg editor like Inkscape.",
+    description="mechaSVG is a python & tk application for creating good-looking energy profile diagrams as Scalable Vector Graphics.",
     long_description=long_description,
 	license="MIT",
     url="https://github.com/ricalmang/mechaSVG",
